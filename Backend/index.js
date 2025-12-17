666e26e22c8353750fc707871d097b63b675954f
Muhammad Abrar <151812244+muhammadAB1@users.noreply.github.com>
2025-12-17 15:56:23 +0500

    Add files via upload

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import user from './MOCK_DATA.json' with { type: 'json' };
 import fs, { readFileSync } from 'fs'
+import { CLIENT_RENEG_LIMIT } from 'tls';
 
 const app = express()
 app.use(express.json())
@@ -31,6 +32,18 @@ app.post('/updateuser', async (req, res) => {
     fs.writeFileSync('./MOCK_DATA.json', JSON.stringify(user))
     res.send('success')
 })
+
+app.get('/updateuserqury', async (req, res) => {
+    const first_name = req.query.first_name;
+    console.log(first_name)
+})
+
+app.get('/user/:id', async (req, res) => {
+    const id = req.params.id
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    const data = await response.json();
+    res.json(data);
+});
 
 
 app.listen(5000, () => {
